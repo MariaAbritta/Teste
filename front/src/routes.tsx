b3bf68643cbf0ba4ca61bcc0792554696e14a45f
Matheus <morinishi1512@gmail.com>
2021-11-30 12:42:40 -0300

    Conseguimos

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,15 +1,26 @@
-import { Route, BrowserRouter , Routes } from "react-router-dom"
+import { Route, BrowserRouter , Routes} from "react-router-dom"
 import Home from "../src/pages/Home/Index" 
 import Clock from "../src/components/Clock/Clock"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useState } from "react"
+import Tabs from "../src/components/Tabs/Tabs"
+import Title from "../src/pages/Home/tittle"
+
 export const Router = () => {
-    return(
+    const [clock, setClock] = useState (1)
+    const [pomodOrcCycles, setPomodOrcCycleType] = useState<number>(1);
+        return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home></Home>}></Route>
-                {/* <Route path="/clock" element={<Clock ></Clock>} ></Route> */}
+                <Route path="/iniciar" element={<Title></Title>} ></Route>
+            </Routes>
+            <Routes>
+
+                <Route path="/" element={<Home/>} />
+                {/* <Route path="/iniciar" element={<Lista></Lista>} ></Route> */}
+                        
             </Routes>
 
+                    
         </BrowserRouter>
     );
 }
